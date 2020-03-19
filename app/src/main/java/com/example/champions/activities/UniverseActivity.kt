fcfbeddccf770cf3115dbf038a157e243e5f088b
thinhtrai1 <ducthinhtrai@gmail.com>
2020-03-19 17:54:10 +0700

    ok

--- app/src/main/java/com/example/champions/activities/UniverseActivity.kt
+++ app/src/main/java/com/example/champions/activities/UniverseActivity.kt
@@ -2,9 +2,13 @@ package com.example.champions.activities
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.transition.Explode
 import android.util.DisplayMetrics
 import android.util.TypedValue
@@ -27,8 +31,11 @@ import kotlinx.android.synthetic.main.dialog_story.*
 
 class UniverseActivity : AppCompatActivity() {
     var mNames = ArrayList<String>()
+    var mNamesHold = ArrayList<String>()
+    lateinit var mAdapter: ArrayAdapter<String>
     var mStories = ArrayList<Story>()
     var mWait = -1
+    lateinit var mPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,30 +53,18 @@ class UniverseActivity : AppCompatActivity() {
         val layout2 = LinearLayout.LayoutParams((metrics.widthPixels - 30.toPx()) / 2, ((metrics.widthPixels - 30.toPx()) / 2) * 1013 / 1800)
         layout2.setMargins(5.toPx())
         layout_2.layoutParams = layout2
-        val layout3 = LinearLayout.LayoutParams((metrics.widthPixels - 30.toPx()) / 2, ((metrics.widthPixels - 30.toPx()) / 2) * 1026 / 1800)
-        layout3.setMargins(5.toPx())
-        layout_3.layoutParams = layout3
+        layout_3.layoutParams = layout2
 
         val rotateAnimation = RotateAnimation(0f, 3600f
             , Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
         rotateAnimation.duration = 2000
         mImage2.startAnimation(rotateAnimation)
 
-        Picasso.get().load("https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fname-of-the-blade-splash.jpg&resize=1800:")
-            .placeholder(R.drawable.image_default).into(mImage1)
-        Picasso.get().load("https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fcomic-series%2Fzedcomic%2Fissue-4-link.jpg&resize=1800:")
-            .placeholder(R.drawable.image_default).into(mImage2)
-        Picasso.get().load("https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fdream-thief-splash.jpg&resize=1800:")
-            .placeholder(R.drawable.image_default).into(mImage3)
-        Picasso.get().load("https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fbow-and-kunai-splash.jpg&resize=1800:")
-            .placeholder(R.drawable.image_default).into(mImage4)
-        Picasso.get().load("https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fsett-color-splash.jpg&resize=1800:")
-            .placeholder(R.drawable.image_default).into(mImage5)
-        Picasso.get().load("https://universe.leagueoflegends.com/images/latestBg_Wallpaper.png")
-            .placeholder(R.drawable.image_default).into(mImage0)
-
-        val namesAdapter = ArrayAdapter(this, R.layout.item_listview_story, mNames)
-        mLvUniverse.adapter = namesAdapter
+        mPref = getSharedPreferences("uni", Context.MODE_PRIVATE)
+        mAdapter = ArrayAdapter(this, R.layout.item_listview_story, mNames)
+        mLvUniverse.adapter = mAdapter
+        mLvUniverse.isFocusable = false
+        load()
 
         var mRef = FirebaseDatabase.getInstance().reference
         mRef.child("Stories").addChildEventListener(object: ChildEventListener {
@@ -87,9 +82,16 @@ class UniverseActivity : AppCompatActivity() {
                 var mStory = p0.getValue(Story::class.java)!!
                 mStories.add(mStory)
                 val size = mStories.size
+                if (size == 5 && mStory.update != mPref.getString("54", "")) {
+                    reSave()
+                }
                 if (size > 5) {
-                    mNames.add("" + (size - 5) + ".  " + mStory.name)
-                    namesAdapter.notifyDataSetChanged()
+                    var name =
+                    if (mStory.champ != null) "" + (size - 5) + ".  " + mStory.name + " [" + mStory.champ + "]"
+                    else "" + (size - 5) + ".  " + mStory.name
+                    mNames.add(name)
+                    mNamesHold.add(name)
+                    mAdapter.notifyDataSetChanged()
                 }
                 if (mStories.size - 1 == mWait)
                     mReadDialog(mStories.size - 1)
@@ -108,6 +110,70 @@ class UniverseActivity : AppCompatActivity() {
         mLvUniverse.setOnItemClickListener { _, _, position, _ ->
             mReadDialog(position + 5)
         }
+
+        mEdtSearch.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                mNames.clear()
+                mNames.addAll(mNamesHold)
+                var temp = ArrayList<String>()
+                for (name: String in mNames)
+                    if (name.toLowerCase().contains(mEdtSearch.text.toString().toLowerCase()))
+                        temp.add(name)
+                mNames.clear()
+                mNames.addAll(temp)
+                mAdapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    private fun reSave() {
+        mPref.edit().putString("11", mStories[0].image).apply()
+        mPref.edit().putString("12", mStories[0].name).apply()
+        mPref.edit().putString("13", mStories[0].sub).apply()
+        mPref.edit().putString("21", mStories[1].image).apply()
+        mPref.edit().putString("22", mStories[1].name).apply()
+        mPref.edit().putString("31", mStories[2].image).apply()
+        mPref.edit().putString("32", mStories[2].name).apply()
+        mPref.edit().putString("41", mStories[3].image).apply()
+        mPref.edit().putString("42", mStories[3].name).apply()
+        mPref.edit().putString("43", mStories[3].sub).apply()
+        mPref.edit().putString("51", mStories[4].image).apply()
+        mPref.edit().putString("52", mStories[4].name).apply()
+        mPref.edit().putString("53", mStories[4].sub).apply()
+        mPref.edit().putString("54", mStories[4].update).apply()
+
+        load()
+    }
+
+    private fun load() {
+        mTvName1.text = mPref.getString("12", "")
+        mTvSub1.text = mPref.getString("13", "")
+        mTvName2.text = mPref.getString("22", "")
+        mTvName3.text = mPref.getString("32", "")
+        mTvName4.text = mPref.getString("42", "")
+        mTvSub4.text = mPref.getString("43", "")
+        mTvName5.text = mPref.getString("52", "")
+        mTvSub5.text = mPref.getString("53", "")
+        Picasso.get().load(mPref.getString("11", "https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fname-of-the-blade-splash.jpg&resize=1800:"))
+            .placeholder(R.drawable.image_default).into(mImage1)
+        Picasso.get().load(mPref.getString("21", "https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fcomic-series%2Fzedcomic%2Fissue-4-link.jpg&resize=1800:"))
+            .placeholder(R.drawable.image_default).into(mImage2)
+        Picasso.get().load(mPref.getString("31", "https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fdream-thief-splash.jpg&resize=1800:"))
+            .placeholder(R.drawable.image_default).into(mImage3)
+        Picasso.get().load(mPref.getString("41", "https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fbow-and-kunai-splash.jpg&resize=1800:"))
+            .placeholder(R.drawable.image_default).into(mImage4)
+        Picasso.get().load(mPref.getString("51", "https://am-a.akamaihd.net/image?f=https%3A%2F%2Funiverse-meeps.leagueoflegends.com%2Fv1%2Fassets%2Fimages%2Fsett-color-splash.jpg&resize=1800:"))
+            .placeholder(R.drawable.image_default).into(mImage5)
+        Picasso.get().load("https://universe.leagueoflegends.com/images/latestBg_Wallpaper.png")
+            .placeholder(R.drawable.image_default).into(mImage0)
+
+        mAdapter.notifyDataSetChanged()
     }
 
     private fun mReadDialog(pos: Int) {
@@ -144,9 +210,12 @@ class UniverseActivity : AppCompatActivity() {
 
     class Story {
         val name = ""
-        var story = ""
-        var image: String? = null
-        var web = 0
+        val story = ""
+        val image: String? = null
+        val web = 0
+        val sub = ""
+        val champ: String? = null
+        val update = ""
         fun Story() {}
     }
 }
